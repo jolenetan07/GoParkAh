f3c020c8000e26d510cb47ff8adba5e77ab3b5e4
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2023-01-15 23:34:13 +0800

    delete irrelevant stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,6 @@ const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
   {
-    path: 'places',
-    loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
     canLoad: [AuthGuard]
